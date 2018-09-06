c5a1c6ab3547866a6c81bd17407c2215669e7185
Emil Koutanov <ekoutanov@gmail.com>
2018-09-06 10:33:02 +1000

    Sample: offsets will be committed even if no records are polled

--- src/test/java/com/obsidiandynamics/jackdaw/sample/ConsumerSample.java
+++ src/test/java/com/obsidiandynamics/jackdaw/sample/ConsumerSample.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.*;
 import org.apache.kafka.common.serialization.*;
 
 import com.obsidiandynamics.jackdaw.*;
+import com.obsidiandynamics.threads.*;
 import com.obsidiandynamics.yconf.util.*;
 import com.obsidiandynamics.zerolog.*;
 
@@ -28,6 +29,7 @@ public final class ConsumerSample {
         .with("value.deserializer", StringDeserializer.class.getName())
         .build();
     
+    final int pollIntervalMillis = 100;
     final int commitIntervalMillis = 1_000;
     long lastCommitTime = 0;
     final Map<TopicPartition, OffsetAndMetadata> offsetsToCommit = new HashMap<>();
@@ -43,19 +45,25 @@ public final class ConsumerSample {
           for (ConsumerRecord<String, String> record : recordsList) {
             zlg.i("  %,d: %s", z -> z.arg(record::offset).arg(record::value));
           }
-          
-          if (System.currentTimeMillis() - lastCommitTime > commitIntervalMillis) {
-            if (offsetsToCommit != null) {
-              zlg.i("committing %s", z -> z.arg(offsetsToCommit));
-              consumer.commitAsync(offsetsToCommit, null);
-              offsetsToCommit.clear();
-              lastCommitTime = System.currentTimeMillis();
-            }
-            final ConsumerRecord<String, String> lastRecord = recordsList.get(recordsList.size() - 1);
-            offsetsToCommit.put(new TopicPartition(lastRecord.topic(), lastRecord.partition()), 
-                                new OffsetAndMetadata(lastRecord.offset()));
+        }
+        
+        if (System.currentTimeMillis() - lastCommitTime > commitIntervalMillis) {
+          if (! offsetsToCommit.isEmpty()) {
+            zlg.i("committing %s", z -> z.arg(offsetsToCommit));
+            consumer.commitAsync(offsetsToCommit, null);
+            offsetsToCommit.clear();
+            lastCommitTime = System.currentTimeMillis();
           }
         }
+        
+        if (! records.isEmpty()) {
+          final List<ConsumerRecord<String, String>> recordsList = list(records);
+          final ConsumerRecord<String, String> lastRecord = recordsList.get(recordsList.size() - 1);
+          offsetsToCommit.put(new TopicPartition(lastRecord.topic(), lastRecord.partition()), 
+                              new OffsetAndMetadata(lastRecord.offset()));
+        }
+        
+        Threads.sleep(pollIntervalMillis);
       }
     }
   }
