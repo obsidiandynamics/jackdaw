37a9fa4e1af25762a39da8f3588693b2c17992ab
Emil Koutanov <ekoutanov@gmail.com>
2018-11-10 19:32:53 +1100

    [Warthog] Next snapshot

--- src/main/java/com/obsidiandynamics/jackdaw/ConsumerPipe.java
+++ src/main/java/com/obsidiandynamics/jackdaw/ConsumerPipe.java
@@ -7,7 +7,6 @@ import java.util.concurrent.*;
 
 import org.apache.kafka.clients.consumer.*;
 
-import com.obsidiandynamics.func.*;
 import com.obsidiandynamics.jackdaw.AsyncReceiver.*;
 import com.obsidiandynamics.worker.*;
 import com.obsidiandynamics.worker.Terminator;
